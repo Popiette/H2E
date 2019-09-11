4c05cb5a99496909e60b2c0fb9eeb7956a2d0d40
Sebouche <sebastian71f@gmail.com>
2019-09-11 09:15:03 +0200

    Create Refocus.cs
    Menu doesn't lose focus when clicking with mouse outside of the menu zone

---
+++ menu/Refocus.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+//Put the focus on the selected button when clicking with mouse
+public class Refocus : MonoBehaviour
+{
+
+    GameObject selected;
+
+    void Update()
+    {
+        if (EventSystem.current.currentSelectedGameObject == null)
+        {
+            Debug.Log("Reselecting first input");
+            EventSystem.current.SetSelectedGameObject(selected);
+        }
+        else
+        {
+            selected = EventSystem.current.currentSelectedGameObject;
+        }
+    }
+}
